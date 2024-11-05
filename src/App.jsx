2fb791a8fe0f33581e8d160076daba5e6a2fd8f9
Alex Funk <alexdagreat04@gmail.com>
2024-11-05 14:57:45 -0500

    Added the nav bar to the top of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Link, Outlet } from "react-router-dom";
+import store from "./assets/store.svg";
 
-function App() {
+export default function App() {
   return (
     <>
       <nav>
         <ul>
           <li>
-            <Link to="home">Home</Link>
+            <Link to="/" className="homeLogoLink" aria-hidden="true" tabIndex="-1"><img src={store} alt="store" /><p>Funk Store</p></Link>
           </li>
           <li>
-            <Link to="shop">Shop</Link>
+            <Link to="home" className="homeLink">Home</Link>
+          </li>
+          <li>
+            <Link to="shop" className="shopLink">Shop</Link>
           </li>
         </ul>
       </nav>
@@ -17,5 +21,3 @@ function App() {
     </>
   );
 }
-
-export default App;
